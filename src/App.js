a9ea3d830d5892c2ea8ce3eb0c139286066e8c6a
w3sl1y <SV73623892@idat.edu.pe>
2021-10-08 22:56:24 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div class="container">
+      <div class="left">
+        <div class="top">
+          <div class="min">We Live By</div>
+          <div class="max">The Design <br/>Process</div>
+        </div>
+          <div class="mid">
+          <div class="flecha">Designs</div>
+          </div>
+        <div class="bot"></div>
+      </div>
+      <div class="right">
+        <div class="up">
+        <div class="form">
+          <h1>Form</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo purus at ullamcorper efficitur. Curabitur efficitur tempus ante, id vestibulum est fermentum sit amet. Integer vel varius ligula, id convallis tellus. Nunc ac elementum lorem, vitae molestie nulla. Donec nisi orci, accumsan a ornare ac, ornare sed diam.</p>
+        </div>
+        <div class="style">
+          <h1>Style</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo purus at ullamcorper efficitur. Curabitur efficitur tempus ante, id vestibulum est fermentum sit amet. Integer vel varius ligula, id convallis tellus. Nunc ac elementum lorem, vitae molestie nulla. Donec nisi orci, accumsan a ornare ac, ornare sed diam.</p>
+        </div>
+        </div>
+        <div class="down">
+        <div class="interaction">
+          <h1>Interaction</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo purus at ullamcorper efficitur. Curabitur efficitur tempus ante, id vestibulum est fermentum sit amet. Integer vel varius ligula, id convallis tellus. Nunc ac elementum lorem, vitae molestie nulla. Donec nisi orci, accumsan a ornare ac, ornare sed diam.</p>
+        </div>
+        <div class="unity">
+          <h1>Unity</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo purus at ullamcorper efficitur. Curabitur efficitur tempus ante, id vestibulum est fermentum sit amet. Integer vel varius ligula, id convallis tellus. Nunc ac elementum lorem, vitae molestie nulla. Donec nisi orci, accumsan a ornare ac, ornare sed diam.</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
